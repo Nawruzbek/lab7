e3935dc097a1cb4c3895ba03cbc2f3d5496e920f
Nawruzbek Ibragimow <nawruzbekibragimow@MacBook-Pro-M5-Nawruzbek.local>
2026-05-21 21:47:44 +0300

    Initial commit: задание 2 жесты на Swift

---
+++ GestureApp/GestureApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestureApp
+//
+//  Created by Nawruzbek Ibragimow on 21.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
